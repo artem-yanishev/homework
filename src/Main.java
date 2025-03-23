138a7116cc6f4d23c966eee58bd632b6c841344d
artem-yanishev <janyshevtema79@gmail.com>
2025-03-23 21:07:44 +0300

    Homework 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = dog + 4.0;
+        cat = cat + 4.0;
+        paper = paper + 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        var frog = 5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = (int) (frog / 3.5);
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        var totalWeight = boxer1 + boxer2;
+        System.out.println("Общая масса "+totalWeight);
+        var differenceWeight = boxer2 - boxer1;
+        System.out.println("Разница в массе "+differenceWeight);
+        var remainderOfDivision = (boxer2 % boxer1);
+        System.out.println(remainderOfDivision);
+        var totalTime  = 640;
+        var time = 8;
+        var totalEmployees = totalTime / time;
+        System.out.println("Всего работников в компании - " +totalEmployees+ " человек");
+        totalTime = time * (totalEmployees + 94);
+        System.out.println("Если в кампании работает на 94 человека больше, чем раньше, то всего " +totalTime+ " часов работы может быть поделено между сотрудниками.");
     }
 }
